d264295f7f61223c03d6319360903b72bb3211f7
alamin6688 <alamin.kzs06@gmail.com>
2024-02-17 04:28:01 +0600

    home screen and playground section's js functionality added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,44 @@
-function play(){
-    console.log('start the game now')
+// function play(){
+//     // step-1: hide the home screen. to hide the screen and the class hidden to the home section
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     // console.log(homeSection.classList)
+
+//     // show the playground 
+//     const playgroundScetion = document.getElementById('play-ground');
+//     playgroundScetion.classList.remove('hidden');
+//     // console.log(playgroundScetion.classList);
+// }
+
+function continueGame(){
+    // step-1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet',alphabet);
+
+    // set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set bg color
+    setBackgroundColorById(alphabet);
 }
+
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
