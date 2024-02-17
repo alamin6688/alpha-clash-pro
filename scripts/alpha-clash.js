76732d82332cf68440e24ede5e9f504ff7155d65
alamin6688 <alamin.kzs06@gmail.com>
2024-02-17 23:59:43 +0600

    more js added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,10 +10,86 @@
 //     // console.log(playgroundScetion.classList);
 // }
 
+function handleKeyboardButtonKeyPress(event){
+    const playerPressed = event.key;
+    console.log('player pressed', playerPressed);
+
+    // stop the game if pressed 'Esc'
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    // console.log(playerPressed, expectedAlphabet);
+
+    // check matched or not
+    if(playerPressed === expectedAlphabet){
+        console.log('you got a point');
+
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+
+
+
+
+        // ---------------------------------------------------
+        //  update score
+        //  1. get the current score
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // console.log(currentScore);
+        // const currentScore = getTextElementValueById('current-score');
+        
+        
+        //  2. increase the score by 1
+        const newScore = currentScore + 1;
+
+        //  3. show the updated score
+        // currentScoreElement.innerText = newScore;
+
+        //  start a new round
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+        // ---------------------------------------------------
+    }
+    else{
+        console.log('Durr vai,right key press koren');
+
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
+
+        if(updatedLife === 0){
+            gameOver();
+        }
+
+        // 1. get the current life number
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+        // 2. reduce the life count
+        // const newLife = currentLife - 1;
+
+        // 3. display the updated life count
+        // currentLifeElement.innerText = newLife;
+    }
+
+}
+// capture keyboard key press
+document.addEventListener('keyup', handleKeyboardButtonKeyPress)
+
+
+
 function continueGame(){
     // step-1: generate a random alphabet
     const alphabet = getARandomAlphabet();
-    console.log('your random alphabet',alphabet);
+    // console.log('your random alphabet',alphabet);
 
     // set randomly generated alphabet to the screen (show it)
     const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -25,12 +101,32 @@ function continueGame(){
 
 
 function play(){
+    // hide everything show only playground
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
+
+    // reset score and life
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
     continueGame();
 }
 
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+    // update final score
+    // 1. get the final score
+    const lastScore = getTextElementValueById('current-score');
+    console.log(lastScore);
+    // 2. show the final/last score on the page
+    setTextElementValueById('last-score', lastScore);
 
+    // clear the last selected alphabet highlight
+    const currentAlphabet = getElementTextById('current-alphabet');
+    // console.log(currentAlphabet);
+    removeBackgroundColorById(currentAlphabet);
+}
 
 
 
